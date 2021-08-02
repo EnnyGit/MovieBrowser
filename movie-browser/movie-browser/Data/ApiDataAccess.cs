190d6c6cb839f2da1edc1274c611726c474e51d4
Enny <46939885+EnnyGit@users.noreply.github.com>
2021-08-02 23:10:11 +0200

    Added GetTopratedMovies method

--- movie-browser/movie-browser/Data/ApiDataAccess.cs
+++ movie-browser/movie-browser/Data/ApiDataAccess.cs
@@ -5,6 +5,8 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using movie_browser.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace movie_browser.Data
 {
@@ -12,26 +14,69 @@ namespace movie_browser.Data
     {
         private readonly IHttpClientFactory _clientFactory;
         private readonly string APIKey = "8e0c2dde51210286d3e656c9b7bc0181";
+        private readonly HttpClient _client;
 
         public ApiDataAccess(IHttpClientFactory clientFactory)
         {
             _clientFactory = clientFactory;
+            _client = _clientFactory.CreateClient("meta");
         }
-        public async Task<Tuple<MovieModel, string>> GetMovie()
+        public async Task<Tuple<MovieModel, string>> GetLatestMovie()
         {
-            var client = _clientFactory.CreateClient("meta");
             string errorString;
             MovieModel movie = null; 
 
             try
             {
-                movie = await client.GetFromJsonAsync<MovieModel>($"latest?api_key={APIKey}");
+                movie = await _client.GetFromJsonAsync<MovieModel>($"latest?api_key={APIKey}");
                 errorString = null;
                 return Tuple.Create(movie, errorString);
             }
             catch (Exception e)
             {
                 errorString = $"There was an error getting the movie: { e.Message }";
+            }
+
+            return Tuple.Create(movie, errorString);
+        }
+
+        public async Task<Tuple<List<MovieModel>, string>> GetTopratedMovies()
+        {
+            string errorString;
+            List<MovieModel> movies = null;
+
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync($"top_rated?api_key={APIKey}");
+                if (response != null)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    movies = JObject.Parse(jsonString)["results"].ToObject<List<MovieModel>>();
+                    return Tuple.Create(movies, "");
+                }
+            }
+            catch (Exception e)
+            {
+                errorString = $"There was an error getting the movie: { e.Message }";
+                return Tuple.Create(movies, errorString);
+            }
+            return null;
+        }
+
+        public async Task<Tuple<MovieModel, string>> GetPopularMovies()
+        {
+            string errorString;
+            MovieModel movie = null;
+
+            try
+            {
+                movie = await _client.GetFromJsonAsync<MovieModel>($"popular?api_key={APIKey}");
+                errorString = null;
+                return Tuple.Create(movie, errorString);
+            }
+            catch (Exception e)
+            {
+                errorString = $"There was an error getting the movies: { e.Message }";
             }
 
             return Tuple.Create(movie, errorString);
--- movie-browser/movie-browser/Data/IApiDataAccess.cs
+++ movie-browser/movie-browser/Data/IApiDataAccess.cs
@@ -1,11 +1,14 @@
 ﻿using movie_browser.Models;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace movie_browser.Data
 {
     public interface IApiDataAccess
     {
-        Task<Tuple<MovieModel, string>> GetMovie();
+        Task<Tuple<MovieModel, string>> GetLatestMovie();
+        Task<Tuple<List<MovieModel>, string>> GetTopratedMovies();
+        Task<Tuple<MovieModel, string>> GetPopularMovies();
     }
 }
